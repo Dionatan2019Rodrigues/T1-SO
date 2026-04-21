8cbf2ff782b7138822bc27fe2032c508f0b46adc
Dionatan Rodrigues <dionatanrodrigues351@gmail.com>
2026-04-21 15:47:15 -0300

    ✨ feat : exercise 06

---
+++ q6-t1-fork.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define N 3
+
+int main(){
+    pid_t f[N], w;
+    int status, i;
+
+    for (i = 0; i < N; i++) {
+        f[i] = fork();
+        printf("PID = %d | PPID = %d | i = %d\n", getpid(), getppid(), i);
+    }
+    
+    sleep(10);
+    
+    for (i = 0; i < N; i++) {
+        if (f[i] != 0)
+            w = waitpid(f[i], &status, 0);
+    }
+
+    return 0;
+}
